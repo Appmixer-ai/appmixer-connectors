ede822df14e832a0676ad613b2d29f0d3da91e10
Jiří Hofman <jiri.hofman@gmail.com>
2025-07-10 15:46:38 +0200

    Add CreateProjectLink and DeleteProjectLink components

---
+++ src/appmixer/sonarqube/core/CreateProjectLink/CreateProjectLink.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+    async receive(context) {
+        const { name, projectId, projectKey, url } = context.messages.in.content;
+        const serverUrl = context.auth.serverUrl.replace(/\/$/, '');
+
+        if (!name) {
+            throw new context.CancelError('name is required');
+        }
+
+        if (!url) {
+            throw new context.CancelError('url is required');
+        }
+
+        // https://sonarqube.example.com/web_api/api/project_links/create
+        const { data } = await context.httpRequest({
+            method: 'POST',
+            url: `${serverUrl}/api/project_links/create`,
+            headers: {
+                'Authorization': `Bearer ${context.auth.apiKey}`
+            },
+            data: new URLSearchParams({
+                name,
+                ...(projectId ? { projectId } : {}),
+                ...(projectKey ? { projectKey } : {}),
+                url
+            }).toString()
+        });
+
+        // Drop the wrapping 'link' object if present
+        if (data && data.link) {
+            return context.sendJson(data.link, 'out');
+        }
+        return context.sendJson(data, 'out');
+    }
+};
