18c6110dafcb0ce62c73ecae354b22f4248ce6d3
Jiří Hofman <jiri.hofman@gmail.com>
2025-07-14 09:43:25 +0200

    Update UpdatedRow.js
    Refactor to remove eslint comments but return the same object

--- src/appmixer/snowflake/db/UpdatedRow/UpdatedRow.js
+++ src/appmixer/snowflake/db/UpdatedRow/UpdatedRow.js
@@ -21,18 +21,18 @@ module.exports = {
         const { schema, table } = context.properties;
         const data = await snowflake.consumeStream(context, schema, table, 'update');
         const mid = data.length / 2;
-        for (let i = 0; i < mid; i++) {
+        for (let i = 0; i < mid; i++) {{
 
-            const {
-                /* eslint-disable-next-line no-unused-vars */
-                METADATA$ACTION: _oldAction, METADATA$ISUPDATE: _oldUpdate, METADATA$ROW_ID: _oldId,
-                ...oldRow
-            } = data[i + mid];
-            const {
-                /* eslint-disable-next-line no-unused-vars */
-                METADATA$ACTION: _updatedAction, METADATA$ISUPDATE: _updatedUpdate, METADATA$ROW_ID: _updatedId,
-                ...updatedRow
-            } = data[i];
+            const oldRow = { ...data[i + mid] };
+            delete oldRow.METADATA$ACTION;
+            delete oldRow.METADATA$ISUPDATE;
+            delete oldRow.METADATA$ROW_ID;
+
+            const updatedRow = { ...data[i] };
+            delete updatedRow.METADATA$ACTION;
+            delete updatedRow.METADATA$ISUPDATE;
+            delete updatedRow.METADATA$ROW_ID;
+
             await context.sendJson({ oldRow, updatedRow }, 'out');
         }
     }
