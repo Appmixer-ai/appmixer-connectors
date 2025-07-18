c9c443dd0c18f7fbdb82487fa614aba2bd00869f
Jiří Hofman <jiri.hofman@gmail.com>
2025-07-18 12:59:51 +0200

    npm run lint -- --fix

--- src/appmixer/clerk/core/ListUsers/ListUsers.js
+++ src/appmixer/clerk/core/ListUsers/ListUsers.js
@@ -1,17 +1,17 @@
 module.exports = {
     async receive(context) {
         const { limit = 10, offset = 0, emailAddress, username, phoneNumber, userId } = context.messages.in;
-        
+
         // Build query parameters
         const queryParams = new URLSearchParams();
-        
+
         if (limit) queryParams.append('limit', limit);
         if (offset) queryParams.append('offset', offset);
         if (emailAddress) queryParams.append('email_address', emailAddress);
         if (username) queryParams.append('username', username);
         if (phoneNumber) queryParams.append('phone_number', phoneNumber);
         if (userId) queryParams.append('user_id', userId);
-        
+
         // Make API request
         const response = await context.httpRequest({
             method: 'GET',
@@ -22,7 +22,7 @@ module.exports = {
             },
             json: true
         });
-        
+
         // Return the results
         return context.sendJson({
             users: response.data,
