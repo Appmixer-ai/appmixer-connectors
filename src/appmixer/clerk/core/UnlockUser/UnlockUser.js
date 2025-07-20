4fce6949ab345bffd3470f8046d550e72a621a09
Jiří Hofman <jiri.hofman@gmail.com>
2025-07-20 22:30:55 +0200

    Add lock/unlock user

---
+++ src/appmixer/clerk/core/UnlockUser/UnlockUser.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+    async receive(context) {
+        const { userId } = context.messages.in.content;
+
+        if (!userId) {
+            throw new Error('User ID is required');
+        }
+
+        // Make API request
+        const { data } = await context.httpRequest({
+            method: 'POST',
+            url: `https://api.clerk.com/v1/users/${userId}/unlock`,
+            headers: {
+                'Authorization': `Bearer ${context.auth.apiKey}`
+            }
+        });
+
+        // Return the result
+        return context.sendJson(data, 'out');
+    }
+};
