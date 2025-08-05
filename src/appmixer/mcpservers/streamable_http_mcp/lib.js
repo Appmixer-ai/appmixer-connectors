2aa49f50e553499cd410750fd86b25a7466b362a
vladimir talas <vladimir.talas@gmail.com>
2025-08-05 13:53:18 +0200

    MCP streamable

---
+++ src/appmixer/mcpservers/streamable_http_mcp/lib.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { Client } = require('@modelcontextprotocol/sdk/client/index.js');
+const { StreamableHTTPClientTransport } = require('@modelcontextprotocol/sdk/client/streamableHttp.js');
+
+module.exports = {
+    mcpCall: async (context, url, headers, method, args) => {
+        const clientName = `mcp-client:${context.flowId}:${context.componentId}:${Math.random().toString(36).substring(7)}`;
+        const client = new Client({ name: clientName, version: '1.0.0' });
+        let headersParsed;
+
+        try {
+            headersParsed = headers ? JSON.parse(headers) : undefined;
+        } catch (error) {
+            throw new context.CancelError('Message property "headers" parse error. ' + error.message);
+        }
+
+        const transport = new StreamableHTTPClientTransport(
+            new URL(mcpUrl),
+            {
+                requestInit: {
+                    headers: headersParsed
+                }
+            }
+        );
+        transport.onclose = async () => {
+            await mcp.close();
+        };
+        transport.onerror = async (err) => {
+            await mcp.close();
+        };
+
+        await client.connect(transport);
+
+        let result;
+        try {
+            result = await client[method].apply(client, args);
+        } finally {
+            await client.close();
+        }
+
+        return result;
+    }
+};
