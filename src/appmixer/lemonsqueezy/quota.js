09a27c521694d1ce72927916c4b692584a00e215
Sayam Nasir <sayam.nasir53@gmail.com>
2025-08-13 18:46:58 +0500

    Revert "icons"
    This reverts commit 27b05bb675cc02caafd80ba898c73aae2be155dd.

--- src/appmixer/lemonsqueezy/quota.js
+++ src/appmixer/lemonsqueezy/quota.js
@@ -6,6 +6,7 @@ module.exports = {
         {
             limit: 5,
             window: 1000,
+            throttling: 'auto',
             queueing: 'fifo',
             resource: 'requests'
         }
