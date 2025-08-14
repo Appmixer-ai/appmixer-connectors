3385553d4e0d6822bf67f42204a9618162ac8ead
Jiří Hofman <jiri.hofman@gmail.com>
2025-08-14 23:25:34 +0200

    refactor sendMessage, test its usage in every component > Add new unit test for component SendProvateChannelMessage. Only assert that sendMessage is called with proper arguments. Focus on options. It should be almost the same as #file:SendChannelMessage.test.js

--- src/appmixer/slack/messages/SendDirectMessage/SendDirectMessage.js
+++ src/appmixer/slack/messages/SendDirectMessage/SendDirectMessage.js
@@ -7,7 +7,7 @@ module.exports = {
 
     async receive(context) {
 
-        let { text, userIds } = context.messages.in.content;
+        let { text, userIds, username, iconUrl } = context.messages.in.content;
         if (userIds.length > 8) {
             throw new context.CancelError('You can send a message to a maximum of 8 users at once');
         }
@@ -23,7 +23,18 @@ module.exports = {
         }
 
         // Then, send the message to the channel.
-        const message = await lib.sendMessage(context, response.channel.id, text, false);
+        const options = {};
+        if (username) options.username = username;
+        if (iconUrl) options.iconUrl = iconUrl;
+        const message = await lib.sendMessage(
+            context,
+            response.channel.id,
+            text,
+            false,
+            undefined,
+            undefined,
+            options
+        );
 
         return context.sendJson(message, 'out');
     }
