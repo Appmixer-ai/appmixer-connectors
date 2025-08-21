f49bc5eca0fed9fbbda8e3ae97b8a30bdaef386e
Sayam Nasir <sayam.nasir53@gmail.com>
2025-08-21 18:05:00 +0500

    Stripe. CreatePaymentIntent and ConfirmPaymentIntent Additional fields added

--- src/appmixer/stripe/core/ConfirmPaymentIntent/ConfirmPaymentIntent.js
+++ src/appmixer/stripe/core/ConfirmPaymentIntent/ConfirmPaymentIntent.js
@@ -3,12 +3,14 @@
 module.exports = {
     async receive(context) {
 
-        const id = context.messages.in.content.payment_intent_id;
-        const paymentMethod = context.messages.in.content.payment_method;
+        const { payment_intent_id: id, payment_method: paymentMethod, returnUrl } = context.messages.in.content;
         // https://stripe.com/docs/api/payment_intents/confirm
         const requestData = {};
         if (paymentMethod) {
             requestData.payment_method = paymentMethod;
+        }
+        if (returnUrl) {
+            requestData.return_url = returnUrl;
         }
 
         const { data } = await context.httpRequest({
