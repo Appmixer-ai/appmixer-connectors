2864a6a114ea8a4929bd9c37d1992d5033055f6f
Zbynek Pelunek <zbynekpelunek@gmail.com>
2025-09-03 06:41:50 +0200

    LINE (patch) - new webhook

--- src/appmixer/line/core/SendPushMessage/SendPushMessage.js
+++ src/appmixer/line/core/SendPushMessage/SendPushMessage.js
@@ -11,6 +11,19 @@ module.exports = {
             };
         });
 
+        const { data: validateData } = await context.httpRequest({
+            method: 'POST',
+            url: 'https://api.line.me/v2/bot/message/validate/push',
+            headers: {
+                'Authorization': `Bearer ${context.auth.channelAccessToken}`
+            },
+            data: {
+                messages: messagesArr
+            }
+        });
+
+        context.log({ step: 'validate', data: validateData });
+
         // https://developers.line.biz/en/reference/messaging-api/#send-push-message
         const { data } = await context.httpRequest({
             method: 'POST',
