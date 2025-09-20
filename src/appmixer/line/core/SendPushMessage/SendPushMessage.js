023ac40deb3f0552a8a55380abb650e556ef0ccc
Zbynek Pelunek <zbynekpelunek@gmail.com>
2025-09-21 01:49:52 +0200

    remove push message validation

--- src/appmixer/line/core/SendPushMessage/SendPushMessage.js
+++ src/appmixer/line/core/SendPushMessage/SendPushMessage.js
@@ -20,23 +20,6 @@ module.exports = {
             };
         });
 
-        // const validateResponse = await context.httpRequest({
-        //     method: 'POST',
-        //     url: 'https://api.line.me/v2/bot/message/validate/push',
-        //     headers: {
-        //         'Authorization': `Bearer ${context.auth.channelAccessToken}`
-        //     },
-        //     data: {
-        //         messages: messagesArr
-        //     }
-        // });
-
-        // context.log({ step: 'validate', response: validateResponse });
-
-        // if (!validateResponse.ok) {
-        //     throw new context.CancelError('Message validation failed.');
-        // }
-
         // https://developers.line.biz/en/reference/messaging-api/#send-push-message
         const { data } = await context.httpRequest({
             method: 'POST',
