f43478716a89ecf1baaf70cf870117077020a65c
Sayam Nasir <sayam.nasir53@gmail.com>
2025-09-22 19:26:05 +0500

    fixes

--- src/appmixer/bigCommerce/core/ListOrderStatus/ListOrderStatus.js
+++ src/appmixer/bigCommerce/core/ListOrderStatus/ListOrderStatus.js
@@ -13,7 +13,7 @@ module.exports = {
             }
         });
 
-        // BigCommerce API returns orders array directly
+        // BigCommerce API returns orders status array directly
         const statuses = Array.isArray(data) ? data : [];
 
         await context.sendJson({ statuses }, 'out');
