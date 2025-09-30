8f61dfd72eb32fd84720222f55b1eaadb7379ad9
vladimir talas <vladimir.talas@gmail.com>
2025-09-30 17:17:58 +0200

    jira

--- src/appmixer/jira/core/GetInstanceInfo/GetInstanceInfo.js
+++ src/appmixer/jira/core/GetInstanceInfo/GetInstanceInfo.js
@@ -1,20 +1,28 @@
 'use strict';
 
-const lib = require('../../lib.generated');
-
 module.exports = {
 
     async receive(context) {
 
-        // https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-server-info/#api-rest-api-3-serverinfo-get
-        const { data } = await context.httpRequest({
-            method: 'GET',
-            url: '/rest/api/3/serverInfo',
-            headers: {
-                'Authorization': `Bearer ${context.auth.apiToken}`
-            }
-        });
+        const { profileInfo: { apiUrl } } = context;
 
-        return context.sendJson(data, 'out');
+        try {
+            // https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-server-info/#api-rest-api-3-serverinfo-get
+            const { data } = await context.httpRequest({
+                method: 'GET',
+                url: `${apiUrl}serverInfo`,
+                headers: {
+                    'Authorization': `Bearer ${context.auth.accessToken}`
+                }
+            });
+
+            return context.sendJson(data, 'out');
+        } catch (error) {
+            if (error.response && error.response.status === 404) {
+                throw new context.CancelError('Jira instance information not found or not accessible.');
+            }
+            throw error;
+        }
     }
 };
+
