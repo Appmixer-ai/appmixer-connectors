346f8a24f61700ce321ab0385088e09d99a3af4b
Vladimir Talas <vladimir.talas@appmixer.ai>
2025-12-09 15:27:55 +0100

    Custom webhook sample

--- src/appmixer/cloudflareWAF/routes.js
+++ src/appmixer/cloudflareWAF/routes.js
@@ -26,4 +26,28 @@ module.exports = (context, options) => {
             }
         }
     });
+
+    context.http.router.register({
+        method: 'POST',
+        // path: '/test',
+        path: '/test/{flowId}/{componentId}',
+        options: {
+            // auth: false, // public,  when auth is false otherwise it requires Appmixer access token
+            payload: {
+                // allow: ['application/json'], // allowed content types, allow any if not provided
+            },
+            handler: async req => {
+                const flowId = req.params.flowId;
+                const componentId = req.params.componentId;
+
+                return await context.triggerComponent(
+                    flowId,
+                    componentId,
+                    req.payload,
+                    {
+                        enqueueOnly: 'true' // 'true' => do not wait fot the response
+                    });
+            }
+        }
+    });
 };
