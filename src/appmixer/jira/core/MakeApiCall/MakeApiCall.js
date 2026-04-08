49bf7988378d2195d7a97af34dc894c1fe68778b
Vero <vero@appmixer.com>
2026-04-08 16:08:24 +0000

    jira: add MakeApiCall component
    Adds a generic MakeApiCall component for Jira connector.
    Allows making arbitrary authorized API calls to the Jira Cloud API
    using OAuth2 authentication. The base URL is automatically derived
    from the user's authenticated Jira Cloud site (profileInfo.apiUrl).
    
    Supports: GET, POST, PUT, PATCH, DELETE methods, query parameters, and JSON body.
    Closes #1459

---
+++ src/appmixer/jira/core/MakeApiCall/MakeApiCall.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+
+    async receive(context) {
+
+        const { url, method, parameters, body } = context.messages.in.content;
+        const { profileInfo, auth } = context;
+
+        // profileInfo.apiUrl = https://api.atlassian.com/ex/jira/{cloudId}/rest/api/3/
+        // Strip trailing slash so path like /issue/ID works cleanly
+        const baseUrl = profileInfo.apiUrl.replace(/\/$/, '');
+        const fullUrl = baseUrl + url;
+
+        const requestOptions = {
+            method,
+            url: fullUrl,
+            headers: {
+                'Authorization': `Bearer ${auth.accessToken}`,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        };
+
+        if (parameters && parameters.length > 0) {
+            requestOptions.params = parameters.reduce((acc, { key, value }) => {
+                if (key) acc[key] = value;
+                return acc;
+            }, {});
+        }
+
+        if (body) {
+            requestOptions.data = JSON.parse(body);
+        }
+
+        const response = await context.httpRequest(requestOptions);
+
+        return context.sendJson({
+            status: response.status,
+            headers: response.headers,
+            body: response.data
+        }, 'out');
+    }
+};
