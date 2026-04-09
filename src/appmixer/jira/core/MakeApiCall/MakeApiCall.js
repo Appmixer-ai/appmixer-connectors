b64312b7c416e73acd643eb5f2a38aab399766fa
Vero <vero@appmixer.com>
2026-04-09 06:32:08 +0000

    jira: add headers input to MakeApiCall component

--- src/appmixer/jira/core/MakeApiCall/MakeApiCall.js
+++ src/appmixer/jira/core/MakeApiCall/MakeApiCall.js
@@ -4,7 +4,7 @@ module.exports = {
 
     async receive(context) {
 
-        const { url, method, parameters, body } = context.messages.in.content;
+        const { url, method, headers, parameters, body } = context.messages.in.content;
         const { profileInfo, auth } = context;
 
         // profileInfo.apiUrl = https://api.atlassian.com/ex/jira/{cloudId}/rest/api/3/
@@ -21,6 +21,12 @@ module.exports = {
                 'Accept': 'application/json'
             }
         };
+
+        if (headers && headers.length > 0) {
+            headers.forEach(({ key, value }) => {
+                if (key) requestOptions.headers[key] = value;
+            });
+        }
 
         if (parameters && parameters.length > 0) {
             requestOptions.params = parameters.reduce((acc, { key, value }) => {
